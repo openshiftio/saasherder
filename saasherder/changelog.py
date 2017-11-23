d1b8bfdda5137f63ddf41887df89087d3622be9b
Jaseem Abid <jaseemabid@gmail.com>
2017-11-23 21:56:59 +0530

    Generate markdown changelog
    This patch add a simple API to generate markdown based changelog for a
    particular sass-openshift context.
    
    Here is a sample run
    
    ```
    In [360]: from saasherder.changelog import Changelog
    In [361]: c = Changelog()
    In [362]: c.main('dsaas', 'master', 'master~4')
    
    04d18c56 Component test improvements (#2234)
    d9bfcc70 fix(deployments): resource-card unsubscribes on destroy
    8c22ac04 chore(docs): remove a dulicate issue_template
    a65b98c0 chore(docs): add issue template to redirect to OSIO repo
    
    959f75f Some cleanups suggested by golint (#1778)
    a19c642 Rework controller link tests (#1769)
    dc5c4b9 Explain what WIT is (#1776)
    7278954 Add "workItemLinks" relationship on work item response (#1775)
    
    8453fc8 Merge pull request #479 from fabric8-services/versionUpdate28b384c4-fb33-4ed6-9090-e4ff5a251acf
    d1dea73 Update fabric8-tenant-che version to 2.0.63
    839c30c Reverse lookup tenant by cluster/namespace (openshiftio/openshiftio#1389)
    bdbb91e Merge branch 'master' into IssueOSIO1389_reverse_lookup_tenant
    611824e Merge pull request #478 from fabric8-services/versionUpdate06df89a5-2c6d-40b4-bb5d-f2820f4b5962
    088bb05 Changing number of parameters in tests from 3 to 5 due to che template update
    8b433f3 Update fabric8-tenant-che version to 2.0.62
    05df58c Merge branch 'master' into IssueOSIO1389_reverse_lookup_tenant
    2be1a3e Add `requestId` and `identityId` to the template variables for the Che migration job POD
    cccdb41 Revert "Also modifies the test file"
    8bde08c Also modifies the test file
    b1352fa Apply review comments and fixing test failures :/
    96f16b8 Reverse lookup tenant by cluster/namespace (openshiftio/openshiftio#1389)
    4422022 Add missing import
    b076a55 Correctly manage the error case when the `sub` claim is not found
    b375326 Fix formatting error returned by gofmt
    3e145a7 fix formatting
    82ea019 syntax error
    80f3081 Fix errors
    252b900 Add `requestId` and `identityId` to the template variables ...
    ```

---
+++ saasherder/changelog.py
@@ -0,0 +1,158 @@
+import yaml
+from itertools import chain
+import os
+import subprocess
+
+
+class Changelog:
+
+    workspace = "_workspace/"
+
+    @staticmethod
+    def context_dir(context):
+        return context + "-services/"
+
+    def path(self, context, service_name):
+        """Get YAML config path for a service"""
+
+        return self.context_dir(context) + service_name + ".yaml"
+
+    def definitions(self, context, service_name):
+        """Get list of services defined in service file"""
+
+        with open(self.path(context, service_name)) as f:
+            parsed = yaml.load(f)
+            return parsed['services']
+
+    def service_names(self, context):
+        """Get names of all services in a context"""
+
+        dir = self.context_dir(context)
+        files = os.listdir(dir)
+
+        return [os.path.splitext(f)[0] for f in files]
+
+    def services(self, context):
+        """Get all services in the context"""
+
+        names = self.service_names(context)
+        dsls = [self.definitions(context, name) for name in names]
+        return list(chain(*dsls))
+
+    @staticmethod
+    def diff(s1, s2):
+        """Return the difference between 2 service lists
+
+        This is not a commutative relationship. We are looking for services in
+        first list which differ from that in second list. a ⊕ b ≠ b ⊕ a.
+
+        This gets messy when the hash refers to a branch or none. Ignoring such
+        services for now.
+        """
+
+        # All the services that changed
+        changed = [service for service in s1 if service not in s2]
+
+        def old(service):
+            cadidates = [s for s in s2 if s['name'] == service['name']]
+            return cadidates[0].get('hash') if cadidates else None
+
+        # Filter changed services for which I cannot get a changelog
+        useful = [s for s in changed if s.get('hash') and old(s)]
+
+        # Now changed services can be new ones or change in hash
+        return [{
+            'name': service['name'],
+            'url': service['url'],
+            'new': service.get('hash', 'master'),  # NOTE: null here is silly
+            'old': old(service)
+        } for service in useful]
+
+    @staticmethod
+    def run(cmd):
+        return subprocess.run(cmd, stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE,
+                              shell=True, check=True)
+
+    def worktree(self, service_name):
+        """Get git worktree path for a service"""
+
+        return self.workspace + service_name
+
+    def _clone(self, service):
+        """Private; use checkout instead"""
+
+        cmd = "git clone {} {}".format(service['url'],
+                                       self.worktree(service['name']))
+        self.run(cmd)
+
+    def _fetch(self, service):
+        """Private; use checkout instead"""
+
+        worktree = self.worktree(service['name'])
+        cmd = "cd {} && git fetch ".format(worktree)
+
+        return self.run(cmd)
+
+    def checkout(self, service, version):
+        """Get a service checked out at specified version"""
+
+        if os.path.exists(self.worktree(service['name'])):
+            self._fetch(service)
+        else:
+            self._clone(service)
+
+        worktree = self.worktree(service['name'])
+        cmd = "cd {} && git checkout {} ".format(worktree, version)
+
+        return self.run(cmd)
+
+    def log(self, service, start, end):
+        """Get the changelog for one service"""
+
+        worktree = self.worktree(service['name'])
+        cmd = "cd {} && git log --oneline {}..{} ".format(worktree, end, start)
+
+        return self.run(cmd).stdout.decode('utf-8')
+
+    @staticmethod
+    def markdown(changelog, start, end):
+        """Pretty print the change log
+
+        The incoming object is a list of `(name, messages)` tuples.
+        """
+
+        header = "# Changelog \n\n## From version {} to {}".format(start, end)
+
+        body = ["## {} \n{} \n".format(name, messages)
+                for (name, messages) in changelog]
+
+        return header + ''.join(body)
+
+    def main(self, context, start, end):
+
+        # Checkout to previous version and get services
+        self.run("git checkout {}".format(end))
+        previous = self.services(context)
+
+        self.run("git checkout {}".format(start))
+        now = self.services(context)
+
+        # Go back to where we were
+        self.run("git checkout master")
+
+        changed = self.diff(previous, now)
+
+        # Fetch all the services that changed
+        for service in changed:
+            self.checkout(service, service['new'])
+
+        # Get the changelog for each service as a `(name, [messages])` tuple
+        changelog = [(service['name'], self.log(service, start, end)) for
+                     service in changed]
+
+        markdown = self.markdown(changelog, start, end)
+
+        print(markdown)
+
+        return 0
