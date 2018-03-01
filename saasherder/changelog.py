dbdad96501633c75fd5d6d03b917193e95e88e28
Jaime Melis <jmelis@redhat.com>
2018-03-01 14:55:27 +0100

    Refactor the code so rendering is separated from changelog

--- saasherder/changelog.py
+++ saasherder/changelog.py
@@ -5,11 +5,70 @@ from dateutil.parser import parse
 
 import logging
 import os
-import re
 import textwrap
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+def run(cmd):
+    logger.info("$ {}".format(cmd))
+    p = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
+    stdout, stderr = p.communicate()
+    if p.returncode != 0:
+        raise Exception("$ {} FAILED with exit code {}"
+                        .format(cmd, p.returncode))
+
+    return stdout.decode('utf-8')
+
+class ChangelogRender(object):
+    def __init__(self, changelog, old, new, url):
+        self.changelog = changelog
+        self.old = old
+        self.new = new
+        self.url = url
+
+    def markdown(self):
+        """Pretty print the change log"""
+
+        # Build the body
+        header = "Context changes: [{old:.7}..{new}]({url}/compare/{old}...{new})\n\n"
+        header = header.format(url=self.url, old=self.old, new=self.new)
+
+        body = [header]
+
+        for service in self.changelog.changed_services:
+            names = ", ".join(service['names'])
+            url = service['url']
+
+            section = """\
+                      **[{names}]({url})**
+
+                      Changes: [{old:.7}..{new:.7}]({url}/compare/{old}...{new})
+
+                      Last updated at {last_changed}
+
+                      {log}"""
+
+            section = textwrap.dedent(section)
+
+            section = section.format(names=names,
+                                     url=url,
+                                     old=service['old'],
+                                     new=service['new'],
+                                     log=self.log(service),
+                                     last_changed=self.last_changed(service))
+            body.append(section)
+
+        return '\n'.join(body)
+
+    def log(self, service):
+        """Get the changelog for one service"""
+        template = '- [%h]({}/commit/%H) %s'.format(service['url'])
+        return self.changelog.service_run(service, "git log --format='{}' {}...{}".format(template, service['old'], service['new']))
+
+    def last_changed(self, service):
+        """Get the commit time for service at specific commit"""
+        return self.changelog.service_run(service, "git log -1 --format=%cd {}".format(service['new'])).strip()
 
 class Changelog(object):
 
@@ -18,8 +77,11 @@ class Changelog(object):
     def __init__(self, parent):
         self.parent = parent
 
-    @staticmethod
-    def diff(now, previous):
+        # Key: url of the service
+        # Value: list of services that have changed
+        self.changed_services = []
+
+    def fetch_diff(self, now, previous):
         """Return the difference between 2 service lists
 
         This is not a commutative relationship. We are looking for services in
@@ -31,7 +93,7 @@ class Changelog(object):
 
         # All the services that changed. We will store only those that exist in
         # now and in previous, for which 'hash' is defined in both
-        changed = []
+        changed_services = {}
         for service_name, service in now.items():
             try:
                 prev_service = previous[service_name]
@@ -42,140 +104,55 @@ class Changelog(object):
                 # or if 'hash' is not defined for either service
                 continue
 
-            if service_hash and prev_service_hash and prev_service_hash != service_hash:
-                changed.append(
-                    dict(
-                        name=service['name'],
-                        url=service['url'],
-                        new=service_hash,
-                        old=prev_service_hash
-                    )
-                )
+            if service_hash is None or prev_service_hash is None:
+                continue
 
-        return changed
+            url = service['url'].rstrip('/')
 
-    @staticmethod
-    def run(cmd):
-        logger.info("$ {}".format(cmd))
-        p = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
-        stdout, stderr = p.communicate()
-        if p.returncode != 0:
-            raise Exception("$ {} FAILED with exit code {}"
-                            .format(cmd, p.returncode))
+            if prev_service_hash != service_hash:
+                if url in changed_services:
+                    changed_services[url]['names'].append(service_name)
+                else:
+                    diff_item = dict(name=service_name,
+                                     names=[service_name],
+                                     url=url,
+                                     new=service_hash,
+                                     old=prev_service_hash)
 
-        return stdout.decode('utf-8')
+                    changed_services[url] = diff_item
 
-    def worktree(self, service_name):
+        self.changed_services = changed_services.values()
+        return self.changed_services
+
+    def worktree(self, service):
         """Get git worktree path for a service"""
+        return self.workspace + service['name']
 
-        return self.workspace + service_name
+    def service_run(self, service, cmd):
+        """Run in the directory of the service"""
+        return run("cd {} && {}".format(self.worktree(service), cmd))
 
     def _clone(self, service):
         """Private; use checkout instead"""
-
-        cmd = "git clone {} {}".format(service['url'],
-                                       self.worktree(service['name']))
-        self.run(cmd)
+        run("git clone {} {}".format(service['url'], self.worktree(service)))
 
     def _fetch(self, service):
         """Private; use checkout instead"""
-
-        worktree = self.worktree(service['name'])
-        cmd = "cd {} && git fetch ".format(worktree)
-
-        return self.run(cmd)
+        self.service_run(service, "git fetch")
 
     def checkout(self, service):
         """Get a service checked out at specified version"""
 
-        name = service['name']
-        version = service['new']
+        service_name = service['name']
 
-        if os.path.exists(self.worktree(name)):
-            logger.info("Found git worktree for {}; updating".format(name))
+        if os.path.exists(self.worktree(service)):
+            logger.info("Found git worktree for {}; updating".format(service_name))
             self._fetch(service)
         else:
-            logger.info("Cloning git worktree for {}".format(name))
+            logger.info("Cloning git worktree for {}".format(service_name))
             self._clone(service)
 
-        worktree = self.worktree(service['name'])
-        cmd = "cd {} && git checkout {} ".format(worktree, version)
-
-        return self.run(cmd)
-
-    def log(self, service, old, new):
-        """Get the changelog for one service"""
-
-        worktree = self.worktree(service['name'])
-        remote = service['url'].strip('/')
-        template = '- [%h](_REMOTE_/commit/%H) %s'
-
-        cmd = ("cd {} && git log --format='{}' {}...{}"
-               .format(worktree, template, old, new))
-
-        return self.run(cmd).replace('_REMOTE_', remote)
-
-    def last_changed(self, service, commit):
-        """Get the commit time for service at specific commit"""
-
-        worktree = self.worktree(service['name'])
-        cmd = "cd {} && git log -1 --format=%cd {}".format(worktree, commit)
-
-        return self.run(cmd).strip()
-
-    @staticmethod
-    def markdown(changelog, old, new, context_url):
-        """Pretty print the change log
-
-        The incoming object is a list of `(name, timestamp, messages)` tuples.
-        """
-
-        # Some services may come from the same URL, in this case let's
-        # deduplicate them
-        dedup_changelog = {}
-        for service, timestamp, messages in changelog:
-            url = service['url']
-            name = service.pop('name', None)
-            if url not in dedup_changelog:
-                dedup_changelog[url] = dict(
-                    name=[name],
-                    service=service,
-                    timestamp=timestamp,
-                    messages=messages
-                )
-            else:
-                dedup_changelog[url]['name'].append(name)
-
-        # Build the body
-        context_url = re.sub("\.git$", "", context_url.strip())
-
-        header = "Context changes: [{old:.7}..{new}]({url}/compare/{old}...{new})\n\n"
-        header = header.format(url=context_url, old=old, new=new)
-
-        body = [header]
-
-        for entry in dedup_changelog.values():
-            name = ", ".join(entry['name'])
-            url = re.sub("/$", "", entry['service']['url'])
-
-            section = """\
-                      **[{name}]({url})**
-
-                      Changes: [{old:.7}..{new:.7}]({url}/compare/{old}...{new})
-
-                      Last updated at {entry[timestamp]}
-
-                      {entry[messages]}"""
-
-            section = textwrap.dedent(section)
-            section = section.format(name=name,
-                                     entry=entry,
-                                     old=entry['service']['old'],
-                                     new=entry['service']['new'],
-                                     url=url)
-            body.append(section)
-
-        return '\n'.join(body)
+        return self.service_run(service, "git checkout {}".format(service['new']))
 
     def generate(self, context, old, new):
 
@@ -191,59 +168,52 @@ class Changelog(object):
         # Where am I right now?
         try:
             # This will fail for detached HEAD
-            branch = self.run("git symbolic-ref --short HEAD").strip()
+            branch = run("git symbolic-ref --short HEAD").strip()
             logger.info("On branch {} before generating changelog"
                         .format(branch))
 
         except Exception:
-            branch = self.run("git rev-parse --short HEAD").strip()
+            branch = run("git rev-parse --short HEAD").strip()
             logger.info("HEAD at {} before generating changelog"
                         .format(branch))
 
         # Resolve commit if a date was passed in 'old'
         try:
             dt_old = parse(old)
-            old = self.run("git rev-list -1 --before='{}' {}".format(dt_old, branch)).strip()
+            old = run("git rev-list -1 --before='{}' {}".format(dt_old, branch)).strip()
         except:
             pass
 
         # Resolve commit if a date was passed in 'new'
         try:
             dt_new = parse(new)
-            new = self.run("git rev-list -1 --before='{}' {}".format(dt_new, branch)).strip()
+            new = run("git rev-list -1 --before='{}' {}".format(dt_new, branch)).strip()
         except:
             pass
 
         # Checkout to previous version and get services
-        self.run("git checkout {}".format(old))
+        run("git checkout {}".format(old))
         previous, _ = self.parent.load_services()
 
-        self.run("git checkout {}".format(new))
+        run("git checkout {}".format(new))
         now, _ = self.parent.load_services()
 
         # Go back to where we were
-        self.run("git checkout {}".format(branch))
+        run("git checkout {}".format(branch))
 
-        changed = self.diff(now, previous)
-        logger.info("{} services changed".format(len(changed)))
+        self.fetch_diff(now, previous)
+
+        logger.info("{} services changed".format(len(self.changed_services)))
 
         # Fetch all the services that changed
-        for service in changed:
+        for service in self.changed_services:
+            # only need to checkout the first
             self.checkout(service)
 
-        # Changelog for each service as `(service, timestamp, messages)` tuple
-        changelog = []
-        for service in changed:
-            changelog.append((
-                service,
-                self.last_changed(service, service['new']),
-                self.log(service, service['old'], service['new'])
-            ))
+        url = run("git remote get-url origin").rstrip("/").rstrip(".git")
 
-        url = self.run("git remote get-url origin")
+        render = ChangelogRender(self, old, new, url)
 
-        markdown = self.markdown(changelog, old, new, url)
-
-        print(markdown)
+        print render.markdown()
 
         return 0
