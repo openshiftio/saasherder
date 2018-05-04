a7cb01c0ee5f003146ad1e9058e2192c07115be5
Jaime Melis <jmelis@redhat.com>
2018-05-04 14:46:05 +0200

    Issue #66: support prod registry in check_image.py

--- check_image.py
+++ check_image.py
@@ -2,7 +2,6 @@ import anymarkup
 import sys
 import requests
 
-
 if len(sys.argv) < 2:
   print("Provide path to the processed OpenShift template")
   sys.exit(1)
@@ -16,21 +15,28 @@ for i in anymarkup.parse_file(f)["items"]:
 
 for i in images:
   image_split = i.split("/")
-  if len(image_split) == 3:
-    registry, repo, name = image_split
-  else:
+
+  if len(image_split) < 3:
     print("WARNING: Registry for image %s is not set, assuming docker.io. Cannot verify image existance there. Skipping." % i)
     continue
+
+  registry = image_split[0]
+  repo = '/'.join(image_split[1:-1])
+  name = image_split[-1]
+
   image, tag = name.split(":")
+
   if not registry.startswith("http"):
     registry = "https://%s" % registry
 
   url = "%s/v2/%s/%s/tags/list" % (registry, repo, image)
   print("Checking %s" % url)
+
   r = requests.get(url)
+
   if r.status_code != 200:
     raise Exception("Got %d from %s" % (r.status_code, url))
-  
+
   if tag in r.json()["tags"]:
     print("OK")
   else:
