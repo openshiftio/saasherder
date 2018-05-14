6ae28ef45a20af3fa9d5126c0e04952396c5423e
Navid Shaikh <nshaikh@redhat.com>
2018-05-14 20:52:50 +0530

    Improves check_image.py repo name processing using library functions

--- check_image.py
+++ check_image.py
@@ -2,42 +2,55 @@ import anymarkup
 import sys
 import requests
 
+from saasherder.lib import split_repo_name
+
+
 if len(sys.argv) < 2:
-  print("Provide path to the processed OpenShift template")
-  sys.exit(1)
+    print("Provide path to the processed OpenShift template")
+    sys.exit(1)
 f = sys.argv[1]
 
 images = []
 for i in anymarkup.parse_file(f)["items"]:
-  if i["kind"] == "DeploymentConfig":
-    for c in i["spec"]["template"]["spec"]["containers"]:
-      images.append(c["image"])
+    if i["kind"] == "DeploymentConfig":
+        for c in i["spec"]["template"]["spec"]["containers"]:
+            images.append(c["image"])
+
 
 for i in images:
-  image_split = i.split("/")
+    # split the repo name into its components
+    parts = split_repo_name(i)
+    # parts= {"registy": $REGISTRY, "image": "$IMAGE:$TAG",
+    #         "tag": "$TAG", "image_name": "$IMAGE_WITHOUT_TAG"}
 
-  if len(image_split) < 3:
-    print("WARNING: Registry for image %s is not set, assuming docker.io. Cannot verify image existance there. Skipping." % i)
-    continue
+    # case where registry name is not given
+    if not parts.get("registry", False):
+        print("WARNING: Registry for image %s is not set, assuming docker.io. "
+              "Cannot verify image existance there. Skipping." % i)
+        continue
 
-  registry = image_split[0]
-  repo = '/'.join(image_split[1:-1])
-  name = image_split[-1]
+    # grab the registry end point of the repository
+    registry = parts.get("registry")
 
-  image, tag = name.split(":")
+    # get the image name without tag of repository
+    image_name = parts.get("image_name")
 
-  if not registry.startswith("http"):
-    registry = "https://%s" % registry
+    # get the tag of repository
+    tag = parts.get("tag")
 
-  url = "%s/v2/%s/%s/tags/list" % (registry, repo, image)
-  print("Checking %s" % url)
+    if not registry.startswith("http"):
+        registry = "https://%s" % registry
 
-  r = requests.get(url)
+    url = "{0}/v2/{1}/tags/list".format(registry, image_name)
 
-  if r.status_code != 200:
-    raise Exception("Got %d from %s" % (r.status_code, url))
+    print("Checking %s" % url)
 
-  if tag in r.json()["tags"]:
-    print("OK")
-  else:
-    raise Exception("Could not find tag %s in registry" % tag)
+    r = requests.get(url)
+
+    if r.status_code != 200:
+        raise Exception("Got %d from %s" % (r.status_code, url))
+
+    if tag in r.json()["tags"]:
+        print("OK")
+    else:
+        raise Exception("Could not find tag %s in registry" % tag)
