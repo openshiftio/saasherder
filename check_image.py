5d14799c96cf42826318ab8fbfe7b86273a7025d
Jaime Melis <j.melis@gmail.com>
2018-07-27 12:42:19 +0200

    Support matching a regex for the image path (#75)

--- check_image.py
+++ check_image.py
@@ -1,19 +1,30 @@
 """
 Checks the existence of a tag in a registry by calling skopeo
+
+Usage: check_image.py file [regex]
+
+If regex is supplied the path of the image must match this regex
 """
 
-import sys
 import os
+import re
 import subprocess
+import sys
 
 import anymarkup
 
-if len(sys.argv) < 2:
+AUTH_FILE = os.path.expanduser('~/skopeo.json')
+
+try:
+    OPENSHIFT_TEMPLATE = sys.argv[1]
+except IndexError:
     print "Provide path to the processed OpenShift template"
     sys.exit(1)
 
-OPENSHIFT_TEMPLATE = sys.argv[1]
-AUTH_FILE = os.path.expanduser('~/skopeo.json')
+try:
+    image_path_pattern = re.compile(sys.argv[2])
+except IndexError:
+    image_path_pattern = None
 
 images = []
 for i in anymarkup.parse_file(OPENSHIFT_TEMPLATE)["items"]:
@@ -22,6 +33,10 @@ for i in anymarkup.parse_file(OPENSHIFT_TEMPLATE)["items"]:
             images.append(c["image"])
 
 for image in images:
+    if image_path_pattern and not image_path_pattern.search(image):
+        print >>sys.stderr, "Image '%s' does not match '%s'." % (image, image_path_pattern.pattern)
+        sys.exit(1)
+
     with open(os.devnull, 'w') as devnull:
         status_code = subprocess.call(
             ['skopeo', 'inspect', '--authfile', AUTH_FILE, 'docker://%s' % (image,)], stdout=devnull)
