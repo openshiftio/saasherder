7199a63b7254ae10a178099bfdeedd233432f6b7
Maor Friedman <maor.friedman@redhat.com>
2019-06-20 13:26:24 +0300

    load environment vars

--- cico_build_deploy.sh
+++ cico_build_deploy.sh
@@ -3,5 +3,12 @@
 
 /bin/bash cico_test.sh
 
-yum -y install git
+set +x
+eval "$(./env-toolkit load -f jenkins-env.json \
+            GIT_COMMIT \
+            QUAY_USERNAME \
+            QUAY_PASSWORD \
+            DEVSHIFT_TAG_LEN)"
+set -x
+
 make build push
